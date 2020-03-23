d79dfe42a8c116152295bfb98abb80b2a4079766
chisom5 <okoyechisom54@gmail.com>
2020-03-24 00:57:06 +0100

    change gitignore file

--- backend/server.js
+++ backend/server.js
@@ -38,6 +38,7 @@ app.put("/image", image.handleImage(db));
 
 app.post("/imageurl", image.handleApiCall());
 
-app.listen(3001, () => {
-  console.log("app is running on port 3001");
+app.set( 'port', ( process.env.PORT || 3001 ));
+app.listen(app.get( 'port' ), () => {
+  console.log('app is running on port 3001 ' + app.get( 'port' ));
 });
